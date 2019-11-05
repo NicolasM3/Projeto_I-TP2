8551f33d4123f521ab9a99dd693d448715d13456
NicolasOliveira <niicck992@gmail.com>
2019-11-05 08:03:46 -0300

    update sem erro

--- Main.java
+++ Main.java
@@ -9,9 +9,13 @@ public class Main
 			System.out.print("Digite o nome do arquivo: ");
 			String arq =  Teclado.getUmString();
 			Matriz coeficientes = Leitor.LerArquivo(new File(arq));
+			for(int i = 0; i < coeficientes.getLinhas(); i++)
+			{
+				for(int j = 0; j < coeficientes.getColunas() - 1; j++)
+					System.out.print(coeficientes.getValor(i, j) + " ");
+				System.out.println();
+			}
 			double[] a = MetodoGauss.Calcular(coeficientes);
-			//for(int i = 0; i < coeficientes.getLinhas(); i++)
-			//	System.out.println(a[i]);
 
 		}
 		catch(Exception ex)
