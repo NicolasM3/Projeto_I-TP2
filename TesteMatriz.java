05d448f5a82bfc7f97cfed1769abb21f466baf34
Nícolas Oliveira <cc19193@g.unicamp.br>
2019-11-07 21:57:05 -0300

    Update TesteMatriz.java

--- TesteMatriz.java
+++ TesteMatriz.java
@@ -1,28 +1,27 @@
-import java.io.*;
-import Matriz.*;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 public class TesteMatriz
 {
-	public static void main (String[] args)
-    {
-		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-
-		for (;;)
+	BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+	for (;;)
+	{
+		System.out.print ("Capacidade desejada para a Agenda: ");
+		try
 		{
-			try
-			{
-				System.out.print ("Capacidade desejada para a Matriz: ");
-				int cap = Integer.parseInt (entrada.readLine ());
-				Matriz m  = new Matriz(cap);
-			}
-			catch (NumberFormatException e)
-			{
-				System.err.println ("Nao foi digitado um numero inteiro\n\n");
-			}
-			catch (Exception e)
-			{
-				System.err.println (e.getMessage()+"\n\n");
-			}
+			System.out.print ("Capacidade desejada para a Matriz: ");
+			int cap = Integer.parseInt (entrada.readLine ());
+			Matriz m  = new Matriz(cap);
+		}
+		catch (IOException e)
+		{}
+		catch (NumberFormatException e)
+		{
+			System.err.println ("Nao foi digitado um numero inteiro\n\n");
+		}
+		catch (Exception e)
+		{
+			System.err.println (e.getMessage()+"\n\n");
 		}
 	}
 }
